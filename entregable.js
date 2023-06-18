4e684cb70cd714e3481e46bab0441e07569454ec
medcg <martindcg88@gmail.com>
2023-06-18 11:52:58 -0300

    cambios

---
+++ entregable.js
@@ -0,0 +1,7 @@
+const enviar= ()=>{
+    document.querySelector('enviar').addEventListener(
+        )
+        "click",function(){const form= document.queryselector("form");
+        
+        }
+}
