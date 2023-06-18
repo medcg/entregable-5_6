122acd834fb9ea00196b9d3bedda650f376f822a
medcg <martindcg88@gmail.com>
2023-06-18 18:00:23 -0300

    cambios 19

--- entregable.js
+++ entregable.js
@@ -1,10 +1,23 @@
-const enviar= ()=>{
-    document.querySelector(".enviar.").addEventListener(
-        )
-        "click",function(){const form= document.queryselector(".formulario.");
-        document.querySelector("input")
-        }
-        const alert function () {
-        alert('thanks for your coments');
-    }
-}
+document.querySelector(".enviar").addEventListener("click", function () {
+    const form = document.querySelector(".formulario");
+    console.log("First name:", form.first_name.value);
+    console.log("Last name:", form.last_name.value);
+    console.log("Email:", form.email_address.value);
+    console.log("Company name:", form.company_name.value);
+    console.log("Phone number:", form.phone_number.value);
+    console.log("Message:", form.message.value);
+    alert(
+        "First name: " +
+        form.first_name.value +
+        "\nLast name: " +
+        form.last_name.value +
+        "\nEmail: " +
+        form.email_address.value +
+        "\nCompany name: " +
+        form.company_name.value +
+        "\nPhone number: " +
+        form.phone_number.value +
+        "\nMessage: " +
+        form.message.value
+    );
+});
